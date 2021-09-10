c6e4a31361898881522eb002f0d1714065923a8a
Dispa <90232952+Dispa1@users.noreply.github.com>
2021-09-10 18:04:34 +0600

    Add files via upload

---
+++ Нахождение мостов графа/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            int N = int.Parse(textBox1.Text);
+            textBox2.Text ="";
+            int[,] matrix = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                matrix[i, i] = 0;
+                for (int j = i + 1; j < N; j++)
+                {
+                    matrix[i, j] = ran.Next(0, 2);
+                    matrix[j, i] = matrix[i, j];
+                }
+            }
+            
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {       
+                    textBox2.Text += matrix[i, j].ToString();      
+                }
+                textBox2.Text += Environment.NewLine;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
